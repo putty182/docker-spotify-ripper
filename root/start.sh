2c1c1e87da04e97f6d7ecc78c88caf10a4121f25
putty182 <putty182@gmail.com>
2021-03-20 15:17:52 +0000

    sigh

--- root/start.sh
+++ root/start.sh
@@ -5,14 +5,11 @@ rm -Rf
 if ! [ -f /config/playlists.txt ]; then
     touch /config/playlists.txt
 fi
+
 if ! [ -f /config/config.ini ]; then
     touch /config/config.ini
 fi
 
 ln -sfT /config/ ~/.spotify-ripper
 
-if [ -z "${0}" ]; then
-    PYTHONIOENCODING=UTF-8 spotify-ripper /config/playlists.txt
-else
-    PYTHONIOENCODING=UTF-8 spotify-ripper "${0}" "$@"
-fi
+PYTHONIOENCODING=UTF-8 spotify-ripper /config/playlists.txt
